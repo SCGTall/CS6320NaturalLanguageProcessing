d966fc8204efadc06e07be674dbd802e4b95f944
Chaoran Li <scgtall@gmail.com>
2021-04-15 00:02:37 -0500

    Update annotator.py

--- Project/annotator.py
+++ Project/annotator.py
@@ -4,11 +4,13 @@ import json
 # init
 FOLDER_PREFIX = "data/"
 MISINFORMATION_TARGETS = "misinformation_targets.json"
+ms_dict = {}  # misinformation targets
 TRAIN_TWEETS = "train_tweets.json"
+tt_dict = {}  # train tweets
 TRAIN_LABELS = "test.json"
+tl_dict = {}  # train labels
 #TRAIN_LABELS = "example_train_labels.json"
 
-ms_dict = {}  # misinformation targets
 with open(FOLDER_PREFIX + MISINFORMATION_TARGETS, mode='r', encoding='utf-8') as f:
     ms_dict = json.loads(f.read())
 
